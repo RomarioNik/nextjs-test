fb3892383c27b2e51dd39a0e6daf27bbac57b760
RomarioNik <115925888+RomarioNik@users.noreply.github.com>
2025-05-29 22:36:48 +0100

    pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  basePath: "/nextjs-test",
+  reactStrictMode: true,
 };
 
 export default nextConfig;
