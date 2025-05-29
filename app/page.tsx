e70cac4b57a1a9f3bc6a18baf09617097a57e5fc
RomarioNik <115925888+RomarioNik@users.noreply.github.com>
2025-05-30 00:23:00 +0100

    link

--- app/page.tsx
+++ app/page.tsx
@@ -67,6 +67,9 @@ export default function Home() {
           />
           Learn
         </a>
+        <a href="https://vercel.com" rel="noopener noreferrer">
+          Some link
+        </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
